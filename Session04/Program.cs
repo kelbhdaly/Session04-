d6dbaddd69a4d0346d6f3ed0d0f8b0cdb332e8d9
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2025-01-29 19:29:18 +0200

    Method OverLoading

--- Session04/Program.cs
+++ Session04/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        #region Overloading
+        //static int SumNumbers(int a , int b)
+        //{
+        //    return a + b;
+        //}
+        //static double SumNumbers(double a, double b)
+        //{
+        //    return a + b;
+        //}
+        //static int SumNumbers(int a, int b ,int c)
+        //{
+        //    return a + b + c;
+        //} 
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region overloading
+
+            #region Method Overloading
+            //int x = 5, y = 10;
+
+            // Console.WriteLine(SumTwoNumbers(x, y));
+
+            #endregion 
+            #endregion
         }
     }
 }
