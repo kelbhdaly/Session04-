56b426f1585025e9fd24baac6ac69611ed7a65c0
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2025-01-30 16:42:58 +0200

    Unary & Binary Operators

--- Session04/Program.cs
+++ Session04/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Session04
+﻿using Session04.overloading;
+
+namespace Session04
 {
     internal class Program
     {
@@ -26,7 +28,54 @@
 
             // Console.WriteLine(SumTwoNumbers(x, y));
 
-            #endregion 
+            #endregion
+
+            #region OPerators OverLoading
+
+            #region Binary Operator
+            //Complex c1 = new Complex();
+            //c1.Real = 5;
+            //c1.Img = 10;
+            ////Console.WriteLine("C1 ={0} ", c1);
+
+            //Complex c2 = new Complex();
+            //c2.Real = 7;
+            //c2.Img = 20;
+            //Console.WriteLine($"C2 = {c2}");
+            //Complex c3;
+            //c3 = c1 + c2;
+            //Console.WriteLine($"C3 = {c3}");
+
+
+            //Console.WriteLine($" C3 - C1 = {c3} - {c1} = {c3 - c1} "); 
+            #endregion
+
+            #region Unary Operators
+            Complex c1 = new Complex();
+            c1.Real = 5;
+            c1.Img = 10;
+
+            Complex c2 = new Complex();
+            c2.Real = 7;
+            c2.Img = 20;
+            Console.WriteLine("C1 ={0} ", c1);
+
+            //Complex c3 ;   
+            //c3 = ++c1;
+            //Console.WriteLine("C1 ={0} ", c1);
+            //Console.WriteLine($"C3 = {c3}");
+
+
+
+            Complex c3;
+            c3 = c1++;
+            Console.WriteLine("C1 ={0} ", c1);
+            Console.WriteLine($"C3 = {c3}");
+            #endregion
+
+
+            #endregion
+
             #endregion
         }
     }
---
+++ Session04/overloading/Complex.cs
@@ -0,0 +1,66 @@
+﻿namespace Session04.overloading
+{
+    //2 + 5i
+    class Complex
+    {
+        #region Properties
+        public int Real { get; set; }
+        public int Img { get; set; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $" {Real} + {Img}i";
+        }
+        #endregion
+
+        #region Operator Overloading
+        //+
+
+        public static Complex operator +(Complex left, Complex right)
+        {
+            //Complex Result = new Complex()
+            //{
+            //    Real = left.Real+right.Real,
+            //    Img = left.Img+right.Img,
+            //};
+            //return Result ;
+
+            return new Complex()
+            {
+                Real = (left?.Real ?? 0) + (right?.Real ?? 0),
+                Img = (left?.Img ?? 0) + (right?.Img ?? 0),
+            };
+        }
+
+        //- 
+        public static Complex operator -(Complex left, Complex right)
+        {
+            return new Complex()
+            {
+                Real = (left?.Real ?? 0) - (right?.Real ?? 0),
+                Img = (left?.Img ?? 0) - (right?.Img ?? 0)
+            };
+        }
+
+        //++
+        public static Complex operator ++(Complex c)
+        {
+            return new Complex()
+            {
+                Real = (c?.Real ?? 0) + 1,
+                Img = (c?.Img ?? 0)
+            };
+        }
+        public static Complex operator --(Complex c)
+        {
+            return new Complex()
+            {
+                Real = (c?.Real ?? 0) - 1,
+                Img = (c?.Img ?? 0)
+            };
+        }
+        #endregion
+    }
+}
