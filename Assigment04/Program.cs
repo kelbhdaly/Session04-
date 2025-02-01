a5294d415d0cb6dcd3be137670bf990032d98ca5
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2025-02-01 20:17:12 +0200

    Q02

--- Assigment04/Program.cs
+++ Assigment04/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Threading;
 using System;
+using System.Drawing;
 
 namespace Assigment04
 {
@@ -20,6 +21,28 @@ namespace Assigment04
             //Console.WriteLine(calculator.ADD(2, 3, 5));
             //Console.WriteLine(calculator.ADD(2.0, 3.5));
             #endregion
+
+            #region Q02
+            /* Create a class named Rectangle with the following constructors:
+             A parameterless constructor that sets the width and height to 0.
+             A constructor that accepts width and height as integers.
+             A constructor that accepts a single integer and sets both width and height to that value.*/
+
+            // parameterless
+            //Rectangle Rect = new Rectangle();
+            //Console.WriteLine(Rect);
+
+            //accepts width and height
+            //Rectangle R01 = new Rectangle(3,3);
+            //Console.WriteLine(R01);
+
+
+            //constructor that accepts a single
+            //Rectangle R01 = new Rectangle(5);
+            //Console.WriteLine(R01);
+            #endregion
+
+
         }
-}
+    }
 }
---
+++ Assigment04/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment04
+{
+    internal class Rectangle
+    {
+       
+        #region Properties
+        public int Width { get; set; }
+        public int Height { get; set; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"Width = {Width} , height = {Height}";
+        }
+        #endregion
+
+        #region Constructors
+        //public Rectangle(int width, int height) // A constructor that accepts width and height as integers
+        //{
+        //    Width = width;
+        //    Height = height;
+        //}
+
+        //public Rectangle(int size)
+        //{
+        //    Width = size;
+        //    Height = size;
+            
+        //}
+        #endregion
+    }
+}
